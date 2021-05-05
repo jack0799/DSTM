c65580af0380f3276e7bf6059342a5e4d41b7757
jack0799 <roshanshah620@gmail.com>
2021-05-05 22:09:38 +0530

    hostag

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 //const bodyParser = require('body-parser');
 require('dotenv/config');
 
