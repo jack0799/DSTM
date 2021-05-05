61fd43def57dc4a055ce86e72c0a93c6dec6077a
jack0799 <roshanshah620@gmail.com>
2021-05-05 20:59:43 +0530

    Added a Procfile.

---
+++ Documents/DSTnode/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+//const bodyParser = require('body-parser');
+require('dotenv/config');
+
+//app.use(bodyParser.json);
+app.use(express.json());
+//import routes
+const postRoutes = require('./routes/posts');
+
+//middle
+//app.use(bodyParser.json);
+app.use('/post', postRoutes);
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Homepage');
+});
+
+app.use('/post', postRoutes);
+
+//db connection
+mongoose.connect(process.env.DB,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log('Connected')
+);
+
+//server
+app.listen(5000);
