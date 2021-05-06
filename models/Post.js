1e7f477780e0f8104b5bf17c66bf165ba8a1f333
jack0799 <roshanshah620@gmail.com>
2021-05-06 12:46:38 +0530

    hostag

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose');
 const { stringify } = require('querystring');
 
-const postSchema = mongoose.Schema({
+const postSchemacc = mongoose.Schema({
+    Charge_Controller_Id: String,
+    Charge_Controller_Status: String,
+    Solar_Input_Current: String,
+    Solar_Input_Power: String,
+    Solar_Input_Voltage: String,
+    Battery_Load_Current: String,
+    Battery_Load_Power: String,
+    Buck_Output_Voltage: String
+});
+const postSchemasscb = mongoose.Schema({
     name: String,
     title: String,
+    desc: String,
+    desc: String,
+    desc: String,
+    desc: String,
     desc: String
 });
 
-module.exports = mongoose.model('Posts', postSchema);
+//module.exports = mongoose.model('Posts', postSchema);
+const CC = mongoose.model('CC', postSchemacc);
+const SSCB = mongoose.model('SSCB', postSchemasscb);
+
+module.exports = { CC, SSCB };
--- routes/posts.js
+++ routes/posts.js
@@ -1,36 +1,61 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/Post');
+const { CC, SSCB } = require('../models/Post');
 
 router.get('/', async (req, res) => {
-    //res.send('Post');
-    try {
-        const posts = await Post.find();
-        res.json(posts);
-    } catch (err) {
-        res.json({ message: err });
-    }
+    res.send('Post');
+    // try {
+    //     const posts = await CC.find();
+    //     res.json(posts);
+    // } catch (err) {
+    //     res.json({ message: err });
+    // }
 });
 
 router.post('/', async (req, res) => {
     console.log(req.body);
-    const post = new Post({
-        name: req.body.name,
-        title: req.body.title,
-        desc: req.body.desc
+    const post = new CC({
+        Charge_Controller_Id: req.body.payload_fields.Charge_Controller_Id,
+        Charge_Controller_Status: req.body.payload_fields.Charge_Controller_Status,
+        Solar_Input_Current: req.body.payload_fields.Solar_Input_Current,
+        Solar_Input_Power: req.body.payload_fields.Solar_Input_Power,
+        Solar_Input_Voltage: req.body.payload_fields.Solar_Input_Voltage,
+        Battery_Load_Current: req.body.payload_fields.Battery_Load_Current,
+        Battery_Load_Power: req.body.payload_fields.Battery_Load_Power,
+        Buck_Output_Voltage: req.body.payload_fields.Buck_Output_Voltage
     });
-    try {
-        const savedPost = await post.save();
-        res.json(savedPost);
-    } catch (err) {
-        res.json({ message: err });
+    const post1 = new SSCB({
+        name: req.body.payload_fields.SSCB_Id,
+        title: req.body.payload_fields.SSCB_Status,
+        desc: req.body.payload_fields.Input_Current,
+        desc: req.body.payload_fields.Output_Current,
+        desc: req.body.payload_fields.Output_Energy,
+        desc: req.body.payload_fields.Output_Power,
+        desc: req.body.payload_fields.Output_Voltage
+    });
+    if (req.body.counter % 4 == 0) {
+        try {
+            const savedPost = await post.save();
+            res.json(savedPost);
+        } catch (err) {
+            res.json({ message: err });
+        }
+
+
     }
-    // post.save().then(data => {
-    //     res.json(data);
-    // }).catch(err => {
-    //     console.log(err);
-    //     res.json({ message: err });
-    // });
+    else {
+
+        try {
+            const savedPost = await post1.save();
+            res.json(savedPost);
+        } catch (err) {
+            res.json({ message: err });
+        }
+
+    }
+
+
+
 });
 
 module.exports = router;
