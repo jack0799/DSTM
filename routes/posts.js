e677d0fc7b857d47d778d304b5f11b0627e56116
jack0799 <roshanshah620@gmail.com>
2021-10-19 15:06:29 +0530

    changes

--- routes/posts.js
+++ routes/posts.js
@@ -15,25 +15,25 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     console.log(req.body);
     const post = new CC({
-        Charge_Controller_Id: req.body.payload_fields.Charge_Controller_Id,
-        Charge_Controller_Status: req.body.payload_fields.Charge_Controller_Status,
-        Solar_Input_Current: req.body.payload_fields.Solar_Input_Current,
-        Solar_Input_Power: req.body.payload_fields.Solar_Input_Power,
-        Solar_Input_Voltage: req.body.payload_fields.Solar_Input_Voltage,
-        Battery_Load_Current: req.body.payload_fields.Battery_Load_Current,
-        Battery_Load_Power: req.body.payload_fields.Battery_Load_Power,
-        Buck_Output_Voltage: req.body.payload_fields.Buck_Output_Voltage
+        Charge_Controller_Id: req.body.uplink_message.decoded_payload.Charge_Controller_Id,
+        Charge_Controller_Status: req.body.uplink_message.decoded_payload.Charge_Controller_Status,
+        Solar_Input_Current: req.body.uplink_message.decoded_payload.Solar_Input_Current,
+        Solar_Input_Power: req.body.uplink_message.decoded_payload.Solar_Input_Power,
+        Solar_Input_Voltage: req.body.uplink_message.decoded_payload.Solar_Input_Voltage,
+        Battery_Load_Current: req.body.uplink_message.decoded_payload.Battery_Load_Current,
+        Battery_Load_Power: req.body.uplink_message.decoded_payload.Battery_Load_Power,
+        Buck_Output_Voltage: req.body.uplink_message.decoded_payload.Buck_Output_Voltage
     });
     const post1 = new SSCB({
-        SSCB_Id: req.body.payload_fields.SSCB_Id,
-        SSCB_Status: req.body.payload_fields.SSCB_Status,
-        Input_Current: req.body.payload_fields.Input_Current,
-        Output_Current: req.body.payload_fields.Output_Current,
-        Output_Energy: req.body.payload_fields.Output_Energy,
-        Output_Power: req.body.payload_fields.Output_Power,
-        Output_Voltage: req.body.payload_fields.Output_Voltage
+        SSCB_Id: req.body.uplink_message.decoded_payload.SSCB_Id,
+        SSCB_Status: req.body.uplink_message.decoded_payload.SSCB_Status,
+        Input_Current: req.body.uplink_message.decoded_payload.Input_Current,
+        Output_Current: req.body.uplink_message.decoded_payload.Output_Current,
+        Output_Energy: req.body.uplink_message.decoded_payload.Output_Energy,
+        Output_Power: req.body.uplink_message.decoded_payload.Output_Power,
+        Output_Voltage: req.body.uplink_message.decoded_payload.Output_Voltage
     });
-    if (req.body.FPort == 1) {
+    if (req.body.uplink_message.Fport == 1) {
         try {
             const savedPost = await post.save();
             res.json(savedPost);
