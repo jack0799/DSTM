6c8b46d4f0de7e2dd64d80c1c14ec475500be618
jack0799 <roshanshah620@gmail.com>
2021-10-19 16:48:25 +0530

    changes

--- models/Post.js
+++ models/Post.js
@@ -2,23 +2,23 @@ const mongoose = require('mongoose');
 const { stringify } = require('querystring');
 
 const postSchemacc = mongoose.Schema({
-    Charge_Controller_Id: String,
-    Charge_Controller_Status: String,
-    Solar_Input_Current: String,
-    Solar_Input_Power: String,
-    Solar_Input_Voltage: String,
-    Battery_Load_Current: String,
-    Battery_Load_Power: String,
-    Buck_Output_Voltage: String
+    Charge_Controller_Id: Float64Array,
+    Charge_Controller_Status: Float64Array,
+    Solar_Input_Current: Float64Array,
+    Solar_Input_Power: Float64Array,
+    Solar_Input_Voltage: Float64Array,
+    Battery_Load_Current: Float64Array,
+    Battery_Load_Power: Float64Array,
+    Buck_Output_Voltage: Float64Array
 });
 const postSchemasscb = mongoose.Schema({
-    SSCB_Id: String,
-    SSCB_Status: String,
-    Input_Current: String,
-    Output_Current: String,
-    Output_Energy: String,
-    Output_Power: String,
-    Output_Voltage: String
+    SSCB_Id: Float64Array,
+    SSCB_Status: Float64Array,
+    Input_Current: Float64Array,
+    Output_Current: Float64Array,
+    Output_Energy: Float64Array,
+    Output_Power: Float64Array,
+    Output_Voltage: Float64Array
 });
 
 //module.exports = mongoose.model('Posts', postSchema);
