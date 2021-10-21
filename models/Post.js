34ba7be3b766e1bb8417d5c164b75ba6a5644fd4
jack0799 <roshanshah620@gmail.com>
2021-10-21 23:44:47 +0530

    changes

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,39 @@
 const mongoose = require('mongoose');
 const { stringify } = require('querystring');
 
-const postSchemacc = mongoose.Schema({
+// const postSchemacc = mongoose.Schema({
+//     Charge_Controller_Id: String,
+//     Charge_Controller_Status: String,
+//     Solar_Input_Current: String,
+//     Solar_Input_Power: String,
+//     Solar_Input_Voltage: String,
+//     Battery_Load_Current: String,
+//     Battery_Load_Power: String,
+//     Buck_Output_Voltage: String
+// });
+// const postSchemasscb = mongoose.Schema({
+//     SSCB_Id: String,
+//     SSCB_Status: String,
+//     Input_Current: String,
+//     Output_Current: String,
+//     Output_Energy: String,
+//     Output_Power: String,
+//     Output_Voltage: String
+// });
+const alambadi_cluster_sscb = mongoose.Schema({
+    SSCB_Id: String,
+    SSCB_Status: String,
+    Input_Current: String,
+    Output_Current: String,
+    Output_Energy: String,
+    Output_Power: String,
+    Output_Voltage: String,
+    Identification: String,
+    F_port: String,
+    F_cnt: String,
+    Time: String
+});
+const alambadi_cluster_cc = mongoose.Schema({
     Charge_Controller_Id: String,
     Charge_Controller_Status: String,
     Solar_Input_Current: String,
@@ -9,20 +41,30 @@ const postSchemacc = mongoose.Schema({
     Solar_Input_Voltage: String,
     Battery_Load_Current: String,
     Battery_Load_Power: String,
-    Buck_Output_Voltage: String
+    Buck_Output_Voltage: String,
+    Identification: String,
+    F_port: String,
+    F_cnt: String,
+    Time: String
 });
-const postSchemasscb = mongoose.Schema({
-    SSCB_Id: String,
-    SSCB_Status: String,
-    Input_Current: String,
-    Output_Current: String,
-    Output_Energy: String,
-    Output_Power: String,
-    Output_Voltage: String
-});
+
 
 //module.exports = mongoose.model('Posts', postSchema);
-const CC = mongoose.model('CC', postSchemacc);
-const SSCB = mongoose.model('SSCB', postSchemasscb);
+// const CC = mongoose.model('CC', postSchemacc);
+// const SSCB = mongoose.model('SSCB', postSchemasscb);
 
-module.exports = { CC, SSCB };
+const sscba1 = mongoose.model('alambadi_a-1_sscb', alambadi_cluster_sscb);
+const sscba2 = mongoose.model('alambadi_a-2_sscb', alambadi_cluster_sscb);
+const sscba3 = mongoose.model('alambadi_a-3_sscb', alambadi_cluster_sscb);
+const sscba4 = mongoose.model('alambadi_a-4_sscb', alambadi_cluster_sscb);
+const sscba5 = mongoose.model('alambadi_a-5_sscb', alambadi_cluster_sscb);
+const sscba6 = mongoose.model('alambadi_a-6_sscb', alambadi_cluster_sscb);
+
+const cca1 = mongoose.model('alambadi_a-1_cc', alambadi_cluster_cc);
+const cca2 = mongoose.model('alambadi_a-2_cc', alambadi_cluster_cc);
+const cca3 = mongoose.model('alambadi_a-3_cc', alambadi_cluster_cc);
+const cca4 = mongoose.model('alambadi_a-4_cc', alambadi_cluster_cc);
+const cca5 = mongoose.model('alambadi_a-5_cc', alambadi_cluster_cc);
+const cca6 = mongoose.model('alambadi_a-6_cc', alambadi_cluster_cc);
+
+module.exports = { sscba1, sscba2, sscba3, sscba4, sscba5, sscba6, cca1, cca2, cca3, cca4, cca5, cca6 };
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { CC, SSCB } = require('../models/Post');
+const { sscba1, sscba2, sscba3, sscba4, sscba5, sscba6, cca1, cca2, cca3, cca4, cca5, cca6 } = require('../models/Post');
 
 router.get('/', async (req, res) => {
     res.send('Post');
@@ -15,7 +15,40 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     console.log(req.body);
 
-    const post = new CC({
+    // const post = new CC({
+    //     Charge_Controller_Id: req.body.uplink_message.decoded_payload.Charge_Controller_Id,
+    //     Charge_Controller_Status: req.body.uplink_message.decoded_payload.Charge_Controller_Status,
+    //     Solar_Input_Current: req.body.uplink_message.decoded_payload.Solar_Input_Current,
+    //     Solar_Input_Power: req.body.uplink_message.decoded_payload.Solar_Input_Power,
+    //     Solar_Input_Voltage: req.body.uplink_message.decoded_payload.Solar_Input_Voltage,
+    //     Battery_Load_Current: req.body.uplink_message.decoded_payload.Battery_Load_Current,
+    //     Battery_Load_Power: req.body.uplink_message.decoded_payload.Battery_Load_Power,
+    //     Buck_Output_Voltage: req.body.uplink_message.decoded_payload.Buck_Output_Voltage,
+    //     Identification: 'CC',
+    //     F_port: req.body.uplink_message.f_port,
+    //     F_cnt: req.body.uplink_message.f_cnt,
+    //     Time: req.body.received_at
+    // });
+    // const post1 = new SSCB({
+    //     SSCB_Id: req.body.uplink_message.decoded_payload.SSCB_Id,
+    //     SSCB_Status: req.body.uplink_message.decoded_payload.SSCB_Status,
+    //     Input_Current: req.body.uplink_message.decoded_payload.Input_Current,
+    //     Output_Current: req.body.uplink_message.decoded_payload.Output_Current,
+    //     Output_Energy: req.body.uplink_message.decoded_payload.Output_Energy,
+    //     Output_Power: req.body.uplink_message.decoded_payload.Output_Power,
+    //     Output_Voltage: req.body.uplink_message.decoded_payload.Output_Voltage,
+    //     Identification: 'SSCB',
+    //     F_port: req.body.uplink_message.f_port,
+    //     F_cnt: req.body.uplink_message.f_cnt,
+    //     Time: req.body.received_at
+    // });
+
+
+
+
+
+
+    const cc1 = new cca1({
         Charge_Controller_Id: req.body.uplink_message.decoded_payload.Charge_Controller_Id,
         Charge_Controller_Status: req.body.uplink_message.decoded_payload.Charge_Controller_Status,
         Solar_Input_Current: req.body.uplink_message.decoded_payload.Solar_Input_Current,
@@ -23,41 +56,337 @@ router.post('/', async (req, res) => {
         Solar_Input_Voltage: req.body.uplink_message.decoded_payload.Solar_Input_Voltage,
         Battery_Load_Current: req.body.uplink_message.decoded_payload.Battery_Load_Current,
         Battery_Load_Power: req.body.uplink_message.decoded_payload.Battery_Load_Power,
-        Buck_Output_Voltage: req.body.uplink_message.decoded_payload.Buck_Output_Voltage
+        Buck_Output_Voltage: req.body.uplink_message.decoded_payload.Buck_Output_Voltage,
+        Identification: 'CC',
+        F_port: req.body.uplink_message.f_port,
+        F_cnt: req.body.uplink_message.f_cnt,
+        Time: req.body.received_at
     });
-    const post1 = new SSCB({
+
+    const cc2 = new cca2({
+        Charge_Controller_Id: req.body.uplink_message.decoded_payload.Charge_Controller_Id,
+        Charge_Controller_Status: req.body.uplink_message.decoded_payload.Charge_Controller_Status,
+        Solar_Input_Current: req.body.uplink_message.decoded_payload.Solar_Input_Current,
+        Solar_Input_Power: req.body.uplink_message.decoded_payload.Solar_Input_Power,
+        Solar_Input_Voltage: req.body.uplink_message.decoded_payload.Solar_Input_Voltage,
+        Battery_Load_Current: req.body.uplink_message.decoded_payload.Battery_Load_Current,
+        Battery_Load_Power: req.body.uplink_message.decoded_payload.Battery_Load_Power,
+        Buck_Output_Voltage: req.body.uplink_message.decoded_payload.Buck_Output_Voltage,
+        Identification: 'CC',
+        F_port: req.body.uplink_message.f_port,
+        F_cnt: req.body.uplink_message.f_cnt,
+        Time: req.body.received_at
+    });
+
+    const cc3 = new cca3({
+        Charge_Controller_Id: req.body.uplink_message.decoded_payload.Charge_Controller_Id,
+        Charge_Controller_Status: req.body.uplink_message.decoded_payload.Charge_Controller_Status,
+        Solar_Input_Current: req.body.uplink_message.decoded_payload.Solar_Input_Current,
+        Solar_Input_Power: req.body.uplink_message.decoded_payload.Solar_Input_Power,
+        Solar_Input_Voltage: req.body.uplink_message.decoded_payload.Solar_Input_Voltage,
+        Battery_Load_Current: req.body.uplink_message.decoded_payload.Battery_Load_Current,
+        Battery_Load_Power: req.body.uplink_message.decoded_payload.Battery_Load_Power,
+        Buck_Output_Voltage: req.body.uplink_message.decoded_payload.Buck_Output_Voltage,
+        Identification: 'CC',
+        F_port: req.body.uplink_message.f_port,
+        F_cnt: req.body.uplink_message.f_cnt,
+        Time: req.body.received_at
+    });
+
+    const cc4 = new cca4({
+        Charge_Controller_Id: req.body.uplink_message.decoded_payload.Charge_Controller_Id,
+        Charge_Controller_Status: req.body.uplink_message.decoded_payload.Charge_Controller_Status,
+        Solar_Input_Current: req.body.uplink_message.decoded_payload.Solar_Input_Current,
+        Solar_Input_Power: req.body.uplink_message.decoded_payload.Solar_Input_Power,
+        Solar_Input_Voltage: req.body.uplink_message.decoded_payload.Solar_Input_Voltage,
+        Battery_Load_Current: req.body.uplink_message.decoded_payload.Battery_Load_Current,
+        Battery_Load_Power: req.body.uplink_message.decoded_payload.Battery_Load_Power,
+        Buck_Output_Voltage: req.body.uplink_message.decoded_payload.Buck_Output_Voltage,
+        Identification: 'CC',
+        F_port: req.body.uplink_message.f_port,
+        F_cnt: req.body.uplink_message.f_cnt,
+        Time: req.body.received_at
+    });
+
+    const cc5 = new cca5({
+        Charge_Controller_Id: req.body.uplink_message.decoded_payload.Charge_Controller_Id,
+        Charge_Controller_Status: req.body.uplink_message.decoded_payload.Charge_Controller_Status,
+        Solar_Input_Current: req.body.uplink_message.decoded_payload.Solar_Input_Current,
+        Solar_Input_Power: req.body.uplink_message.decoded_payload.Solar_Input_Power,
+        Solar_Input_Voltage: req.body.uplink_message.decoded_payload.Solar_Input_Voltage,
+        Battery_Load_Current: req.body.uplink_message.decoded_payload.Battery_Load_Current,
+        Battery_Load_Power: req.body.uplink_message.decoded_payload.Battery_Load_Power,
+        Buck_Output_Voltage: req.body.uplink_message.decoded_payload.Buck_Output_Voltage,
+        Identification: 'CC',
+        F_port: req.body.uplink_message.f_port,
+        F_cnt: req.body.uplink_message.f_cnt,
+        Time: req.body.received_at
+    });
+
+    const cc6 = new cca6({
+        Charge_Controller_Id: req.body.uplink_message.decoded_payload.Charge_Controller_Id,
+        Charge_Controller_Status: req.body.uplink_message.decoded_payload.Charge_Controller_Status,
+        Solar_Input_Current: req.body.uplink_message.decoded_payload.Solar_Input_Current,
+        Solar_Input_Power: req.body.uplink_message.decoded_payload.Solar_Input_Power,
+        Solar_Input_Voltage: req.body.uplink_message.decoded_payload.Solar_Input_Voltage,
+        Battery_Load_Current: req.body.uplink_message.decoded_payload.Battery_Load_Current,
+        Battery_Load_Power: req.body.uplink_message.decoded_payload.Battery_Load_Power,
+        Buck_Output_Voltage: req.body.uplink_message.decoded_payload.Buck_Output_Voltage,
+        Identification: 'CC',
+        F_port: req.body.uplink_message.f_port,
+        F_cnt: req.body.uplink_message.f_cnt,
+        Time: req.body.received_at
+    });
+
+
+    const sscb1 = new SSCB({
         SSCB_Id: req.body.uplink_message.decoded_payload.SSCB_Id,
         SSCB_Status: req.body.uplink_message.decoded_payload.SSCB_Status,
         Input_Current: req.body.uplink_message.decoded_payload.Input_Current,
         Output_Current: req.body.uplink_message.decoded_payload.Output_Current,
         Output_Energy: req.body.uplink_message.decoded_payload.Output_Energy,
         Output_Power: req.body.uplink_message.decoded_payload.Output_Power,
-        Output_Voltage: req.body.uplink_message.decoded_payload.Output_Voltage
+        Output_Voltage: req.body.uplink_message.decoded_payload.Output_Voltage,
+        Identification: 'SSCB',
+        F_port: req.body.uplink_message.f_port,
+        F_cnt: req.body.uplink_message.f_cnt,
+        Time: req.body.received_at
     });
+    const sscb2 = new SSCB({
+        SSCB_Id: req.body.uplink_message.decoded_payload.SSCB_Id,
+        SSCB_Status: req.body.uplink_message.decoded_payload.SSCB_Status,
+        Input_Current: req.body.uplink_message.decoded_payload.Input_Current,
+        Output_Current: req.body.uplink_message.decoded_payload.Output_Current,
+        Output_Energy: req.body.uplink_message.decoded_payload.Output_Energy,
+        Output_Power: req.body.uplink_message.decoded_payload.Output_Power,
+        Output_Voltage: req.body.uplink_message.decoded_payload.Output_Voltage,
+        Identification: 'SSCB',
+        F_port: req.body.uplink_message.f_port,
+        F_cnt: req.body.uplink_message.f_cnt,
+        Time: req.body.received_at
+    });
+    const sscb3 = new SSCB({
+        SSCB_Id: req.body.uplink_message.decoded_payload.SSCB_Id,
+        SSCB_Status: req.body.uplink_message.decoded_payload.SSCB_Status,
+        Input_Current: req.body.uplink_message.decoded_payload.Input_Current,
+        Output_Current: req.body.uplink_message.decoded_payload.Output_Current,
+        Output_Energy: req.body.uplink_message.decoded_payload.Output_Energy,
+        Output_Power: req.body.uplink_message.decoded_payload.Output_Power,
+        Output_Voltage: req.body.uplink_message.decoded_payload.Output_Voltage,
+        Identification: 'SSCB',
+        F_port: req.body.uplink_message.f_port,
+        F_cnt: req.body.uplink_message.f_cnt,
+        Time: req.body.received_at
+    });
+    const sscb4 = new SSCB({
+        SSCB_Id: req.body.uplink_message.decoded_payload.SSCB_Id,
+        SSCB_Status: req.body.uplink_message.decoded_payload.SSCB_Status,
+        Input_Current: req.body.uplink_message.decoded_payload.Input_Current,
+        Output_Current: req.body.uplink_message.decoded_payload.Output_Current,
+        Output_Energy: req.body.uplink_message.decoded_payload.Output_Energy,
+        Output_Power: req.body.uplink_message.decoded_payload.Output_Power,
+        Output_Voltage: req.body.uplink_message.decoded_payload.Output_Voltage,
+        Identification: 'SSCB',
+        F_port: req.body.uplink_message.f_port,
+        F_cnt: req.body.uplink_message.f_cnt,
+        Time: req.body.received_at
+    });
+    const sscb5 = new SSCB({
+        SSCB_Id: req.body.uplink_message.decoded_payload.SSCB_Id,
+        SSCB_Status: req.body.uplink_message.decoded_payload.SSCB_Status,
+        Input_Current: req.body.uplink_message.decoded_payload.Input_Current,
+        Output_Current: req.body.uplink_message.decoded_payload.Output_Current,
+        Output_Energy: req.body.uplink_message.decoded_payload.Output_Energy,
+        Output_Power: req.body.uplink_message.decoded_payload.Output_Power,
+        Output_Voltage: req.body.uplink_message.decoded_payload.Output_Voltage,
+        Identification: 'SSCB',
+        F_port: req.body.uplink_message.f_port,
+        F_cnt: req.body.uplink_message.f_cnt,
+        Time: req.body.received_at
+    });
+    const sscb6 = new SSCB({
+        SSCB_Id: req.body.uplink_message.decoded_payload.SSCB_Id,
+        SSCB_Status: req.body.uplink_message.decoded_payload.SSCB_Status,
+        Input_Current: req.body.uplink_message.decoded_payload.Input_Current,
+        Output_Current: req.body.uplink_message.decoded_payload.Output_Current,
+        Output_Energy: req.body.uplink_message.decoded_payload.Output_Energy,
+        Output_Power: req.body.uplink_message.decoded_payload.Output_Power,
+        Output_Voltage: req.body.uplink_message.decoded_payload.Output_Voltage,
+        Identification: 'SSCB',
+        F_port: req.body.uplink_message.f_port,
+        F_cnt: req.body.uplink_message.f_cnt,
+        Time: req.body.received_at
+    });
+
+
+
+
+
+
+
+
+
+
     if (req.body.uplink_message.f_port == 1) {
 
-        try {
-            const savedPost = await post.save();
-            res.json(savedPost);
-            console.log(req.body.uplink_message.decoded_payload.Charge_Controller_Id);
-        } catch (err) {
-            console.log(err);
-            res.json({ message: err });
+        if (req.body.uplink_message.end_device_ids == 'cluster-a1') {
+            try {
+                const savedPost = await cc1.save();
+                res.json(savedPost);
+                console.log(req.body.uplink_message.decoded_payload.Charge_Controller_Id);
+            } catch (err) {
+                console.log(err);
+                res.json({ message: err });
+            }
+
         }
+        else if (req.body.uplink_message.end_device_ids == 'cluster-a2') {
+            try {
+                const savedPost = await cc2.save();
+                res.json(savedPost);
+                console.log(req.body.uplink_message.decoded_payload.Charge_Controller_Id);
+            } catch (err) {
+                console.log(err);
+                res.json({ message: err });
+            }
+
+        }
+        else if (req.body.uplink_message.end_device_ids == 'cluster-a3') {
+            try {
+                const savedPost = await cc3.save();
+                res.json(savedPost);
+                console.log(req.body.uplink_message.decoded_payload.Charge_Controller_Id);
+            } catch (err) {
+                console.log(err);
+                res.json({ message: err });
+            }
+
+        }
+        else if (req.body.uplink_message.end_device_ids == 'cluster-a4') {
+            try {
+                const savedPost = await cc4.save();
+                res.json(savedPost);
+                console.log(req.body.uplink_message.decoded_payload.Charge_Controller_Id);
+            } catch (err) {
+                console.log(err);
+                res.json({ message: err });
+            }
+
+        }
+        else if (req.body.uplink_message.end_device_ids == 'cluster-a5') {
+            try {
+                const savedPost = await cc5.save();
+                res.json(savedPost);
+                console.log(req.body.uplink_message.decoded_payload.Charge_Controller_Id);
+            } catch (err) {
+                console.log(err);
+                res.json({ message: err });
+            }
+
+        }
+        else if (req.body.uplink_message.end_device_ids == 'cluster-a6') {
+            try {
+                const savedPost = await cc6.save();
+                res.json(savedPost);
+                console.log(req.body.uplink_message.decoded_payload.Charge_Controller_Id);
+            } catch (err) {
+                console.log(err);
+                res.json({ message: err });
+            }
+
+        }
+
+        // try {
+        //     const savedPost = await post.save();
+        //     res.json(savedPost);
+        //     console.log(req.body.uplink_message.decoded_payload.SSCB_Id);
+        // } catch (err) {
+        //     console.log(err);
+        //     res.json({ message: err });
+        // }
+
+
 
 
     }
     else {
 
+        if (req.body.uplink_message.end_device_ids == 'cluster-a1') {
+            try {
+                const savedPost = await sscb1.save();
+                res.json(savedPost);
+                console.log(req.body.uplink_message.decoded_payload.Charge_Controller_Id);
+            } catch (err) {
+                console.log(err);
+                res.json({ message: err });
+            }
 
-        try {
-            const savedPost = await post1.save();
-            res.json(savedPost);
-            console.log(req.body.uplink_message.decoded_payload.SSCB_Id);
-        } catch (err) {
-            console.log(err);
-            res.json({ message: err });
         }
+        else if (req.body.uplink_message.end_device_ids == 'cluster-a2') {
+            try {
+                const savedPost = await sscb2.save();
+                res.json(savedPost);
+                console.log(req.body.uplink_message.decoded_payload.Charge_Controller_Id);
+            } catch (err) {
+                console.log(err);
+                res.json({ message: err });
+            }
+
+        }
+        else if (req.body.uplink_message.end_device_ids == 'cluster-a3') {
+            try {
+                const savedPost = await sscb3.save();
+                res.json(savedPost);
+                console.log(req.body.uplink_message.decoded_payload.Charge_Controller_Id);
+            } catch (err) {
+                console.log(err);
+                res.json({ message: err });
+            }
+
+        }
+        else if (req.body.uplink_message.end_device_ids == 'cluster-a4') {
+            try {
+                const savedPost = await sscb4.save();
+                res.json(savedPost);
+                console.log(req.body.uplink_message.decoded_payload.Charge_Controller_Id);
+            } catch (err) {
+                console.log(err);
+                res.json({ message: err });
+            }
+
+        }
+        else if (req.body.uplink_message.end_device_ids == 'cluster-a5') {
+            try {
+                const savedPost = await sscb5.save();
+                res.json(savedPost);
+                console.log(req.body.uplink_message.decoded_payload.Charge_Controller_Id);
+            } catch (err) {
+                console.log(err);
+                res.json({ message: err });
+            }
+
+        }
+        else if (req.body.uplink_message.end_device_ids == 'cluster-a6') {
+            try {
+                const savedPost = await sscb6.save();
+                res.json(savedPost);
+                console.log(req.body.uplink_message.decoded_payload.Charge_Controller_Id);
+            } catch (err) {
+                console.log(err);
+                res.json({ message: err });
+            }
+
+        }
+
+
+        // try {
+        //     const savedPost = await post1.save();
+        //     res.json(savedPost);
+        //     console.log(req.body.uplink_message.decoded_payload.SSCB_Id);
+        // } catch (err) {
+        //     console.log(err);
+        //     res.json({ message: err });
+        // }
 
     }
 
